4c1ea00a972f53f58c34eb92cec5c45a2b2fd805
Agageldi Beghanov <agabegh@gmail.com>
2020-04-10 08:38:32 -0800

    first commit
    Added Unity project to GitHub

---
+++ flappy_bird/Assets/scripts/birdScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class birdScript : MonoBehaviour
+{
+    public float speed;
+    public float powerJump;
+    public int score;
+    public GameObject canvas;
+    public AudioClip[] voiceFiles;
+    public Text record;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       canvas.SetActive(false);
+       Time.timeScale = 1;
+       score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * powerJump);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "trigger")
+        {
+
+            col.gameObject.transform.parent.root.gameObject.GetComponent<path>().isTouch = true;
+            score++;
+            GetComponent<AudioSource>().PlayOneShot(voiceFiles[1], 1);
+           
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "obstacle")
+        {
+            EndGame();
+        }
+    }
+    void EndGame()
+    {
+        Time.timeScale = 0;
+        GetComponent<AudioSource>().PlayOneShot(voiceFiles[2], 1);
+        canvas.SetActive(true);
+        if (score > PlayerPrefs.GetInt("Record"))
+        {
+            PlayerPrefs.SetInt("Record", score);
+        }
+
+
+        scoreText.text = score.ToString();
+        record.text = PlayerPrefs.GetInt("Record").ToString();
+    }
+
+
+
+public void Replay()
+    {
+        Application.LoadLevel("game");
+    }
+
+}
---
+++ flappy_bird/Assets/scripts/path.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class path : MonoBehaviour
+
+{
+    public bool isTouch;
+
+    public GameObject pipes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTouch = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouch)
+        {
+           
+            Invoke("nextAssign", 5f);
+            isTouch = false;
+        }
+
+    }
+    void nextAssign()
+    {
+        transform.position = transform.position + new Vector3(14, 0, 0);
+
+        float yPos = Random.Range(-4f, 1f);
+        Debug.Log(yPos.ToString());
+
+        pipes.transform.localPosition = new Vector3(0, yPos, 0);
+
+
+    }
+}
